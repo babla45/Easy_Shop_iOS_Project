d530955744a41f378e1e1f0deff9f34256b5bc75
Md Babla Islam <bilasislam5643@gmail.com>
2025-01-02 13:41:44 +0600

    firebase auth added

---
+++ Easy_ShopTests/Easy_ShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  Easy_ShopTests.swift
+//  Easy_ShopTests
+//
+//  Created by Dibyo sarkar on 2/1/25.
+//
+
+import Testing
+@testable import Easy_Shop
+
+struct Easy_ShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
